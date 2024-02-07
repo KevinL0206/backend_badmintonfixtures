92fb5e801c4a35482dce45b6b4ab4d7a8317d31d
Your GitHub Username <k-lam02@outlook.com>
2024-02-07 20:53:44 +0000

    Refactor saving player instance in ClubPlayersDisplayCreateView

--- mainapp/views.py
+++ mainapp/views.py
@@ -59,7 +59,7 @@ class ClubPlayersDisplayCreateView(APIView): # this class will display all the p
         print(serializer.is_valid())
         if serializer.is_valid():
             
-            playerInstance = serializer.save(club=clubInstance)
+            playerInstance = serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
