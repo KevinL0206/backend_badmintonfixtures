3d1d6aaae0ea1d6bea6dfa9e676dab68af5725bc
Your GitHub Username <k-lam02@outlook.com>
2024-02-08 11:43:55 +0000

    Refactor UpdateMatchView to reset player recentlyPlayed flag

--- mainapp/views.py
+++ mainapp/views.py
@@ -275,12 +275,12 @@ class CreateMatchView(APIView): # this class will create a match
 class UpdateMatchView(APIView): # this class will update a match
 
     def post(self,request,username,clubname,year, month, day,matchID,format=None):
-        """
+        
         if not request.user.is_authenticated:
             return Response({'message': 'Unauthorized'}, status=401)
         if request.user.username != username:
             return Response({'message': 'Unauthorized'}, status=401)
-        """
+        
         serializer = UpdateMatchSerializer(data=request.data)
         currentUser = username
         sessiondate = timezone.datetime(int(year),int(month),int(day))
@@ -295,6 +295,7 @@ class UpdateMatchView(APIView): # this class will update a match
         if len(list(sessionInstance.players.filter(inGameFlag=False, recentlyPlayed=False))) == len(players):
             for playername in players:
                 playername.recentlyPlayed = False
+                playername.save()
 
         team1 = matchInstance.team1.all()
         team2 = matchInstance.team2.all()
